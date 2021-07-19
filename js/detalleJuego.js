16bf52f95f095fc20778d21b934caeb08cb1500b
Carlos Muñoz <71146745+Codigo-Carlos@users.noreply.github.com>
2021-07-19 12:37:21 -0400

    Update detalleJuego.js

--- js/detalleJuego.js
+++ js/detalleJuego.js
@@ -38,7 +38,7 @@ new Vue({
                     this.juegos=data;
                     this.calificacionjuego = this.calificacionAestrellas(this.juegos.calificacion);
                     this.calificacionusr = this.calificacionAestrellas(this.juegos.calificacionusr);
-                    this.juegos.calificacion = parseFloat(this.data.calificacion.toFixed(1));
+                    this.juegos.calificacion = round(this.data.calificacion, 1);
                     vm.$forceUpdate();
             }catch(error){
 
