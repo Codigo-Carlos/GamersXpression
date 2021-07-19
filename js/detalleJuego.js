b5f87142996baa60a79e38fe15aa4600c8c6333e
Carlos Muñoz <71146745+Codigo-Carlos@users.noreply.github.com>
2021-07-19 13:00:56 -0400

    Update detalleJuego.js

--- js/detalleJuego.js
+++ js/detalleJuego.js
@@ -36,7 +36,6 @@ new Vue({
            
                     const data = await resp.json();
                     this.juegos=data;
-                    this.juegos.calificacion = parseFloat(this.data.calificacion).toFixed(1);
                     this.calificacionjuego = this.calificacionAestrellas(this.juegos.calificacion);
                     this.calificacionusr = this.calificacionAestrellas(this.juegos.calificacionusr);
                     
